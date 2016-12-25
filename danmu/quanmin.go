d213e6839a49f346ffa300b7c45e8f08a134a7b8
yxd123 <176912231@163.com>
2016-12-25 11:29:49 +0800

    complate quanmin danmu

--- danmu/quanmin.go
+++ danmu/quanmin.go
@@ -1,9 +1,13 @@
 package danmu
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"log"
+	"net"
+	"regexp"
 	"strconv"
 	"time"
 )
@@ -20,13 +24,14 @@ type QuanminClient struct {
 
 func (q *QuanminClient) Run() {
 	q.loadConfig()
+	q.initSocket()
 }
 
 func (q *QuanminClient) loadConfig() {
 	url := fmt.Sprintf(configUrl, int(time.Now().Unix()))
 	buf, _ := HttpGet(url, nil)
 
-	log.Println(buf)
+	log.Println("load config:", buf)
 
 	a := int32(binary.BigEndian.Uint32(buf[0:4])) ^ 172
 	b := int32(binary.BigEndian.Uint32(buf[4:8])) ^ 172
@@ -42,6 +47,80 @@ func (q *QuanminClient) loadConfig() {
 		q.uid = z
 	} else {
 		buf, _ = HttpGet(fmt.Sprintf(zzzzUrl, roomId, int(time.Now().Unix()/50)), nil)
-		log.Println(string(buf))
+		js, _ := simplejson.NewJson(buf)
+		q.uid, _ = js.Get("uid").Int()
+	}
+}
+
+func (q *QuanminClient) genWriteBuffer() bytes.Buffer {
+	var buffer bytes.Buffer
+	buffer.WriteString("{\n")
+	buffer.WriteString("   \"os\" : 135,\n")
+	buffer.WriteString("   \"pid\" : 10003,\n")
+	buffer.WriteString(fmt.Sprintf("   \"rid\" : \"%d\",\n", q.uid))
+	buffer.WriteString("   \"timestamp\" : 78,\n")
+	buffer.WriteString("   \"ver\" : 147\n}")
+
+	length := len(buffer.Bytes())
+	int32buf := new(bytes.Buffer)
+	binary.Write(int32buf, binary.BigEndian, uint32(length))
+
+	var msg bytes.Buffer
+	msg.Write(int32buf.Bytes())
+	msg.Write(buffer.Bytes())
+	msg.Write([]byte{0x0a})
+
+	return msg
+}
+
+func (q *QuanminClient) initSocket() {
+	log.Println("初始化网络连接 for Quanmin")
+
+	data := q.genWriteBuffer()
+
+	addr, _ := net.ResolveTCPAddr("tcp4", fmt.Sprintf("%s:%d", q.ip, q.port))
+	conn, _ := net.DialTCP("tcp", nil, addr)
+	conn.Write(data.Bytes())
+
+	recvBuffer := make([]byte, 2048)
+	for {
+		n, err := conn.Read(recvBuffer)
+		if n == 0 || err != nil {
+			continue
+		}
+
+		q.parse(recvBuffer)
+	}
+}
+
+func (q *QuanminClient) parse(data []byte) {
+	reg := regexp.MustCompile(`({"ver".*?"cid":1})`)
+	contents := reg.FindAllString(string(data), -1)
+	for _, item := range contents {
+		js, err := simplejson.NewJson([]byte(item))
+		if err != nil {
+			log.Println("json error", err)
+			continue
+		}
+		json, err := js.Get("chat").Get("json").String()
+		if err != nil {
+			log.Println("json err", err, string(item))
+			continue
+		}
+
+		js, _ = simplejson.NewJson([]byte(json))
+		nick, err := js.Get("user").Get("nick").String()
+		if err != nil {
+			log.Println("parse nick error", err, string(item))
+			continue
+		}
+
+		text, err := js.Get("text").String()
+		if err != nil {
+			log.Println("parse nick error", err, string(item))
+			continue
+		}
+
+		log.Println(nick, text)
 	}
 }
--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	danmu := danmu.New(c)
 	// danmu.Add("http://www.panda.tv/573130")
 	// danmu.Add("http://www.douyu.com/976537")
-	danmu.Add("http://www.quanmin.tv/v/15")
+	danmu.Add("http://www.quanmin.tv/555")
 	danmu.Run()
 
 	for i := 0; i < 2; i++ {
