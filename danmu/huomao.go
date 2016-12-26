f105bfdf3454d9032d12e328c5293bf5cc5ee82e
yxd123 <176912231@163.com>
2016-12-26 23:07:22 +0800

    add huomao

---
+++ danmu/huomao.go
@@ -0,0 +1,25 @@
+package danmu
+
+import (
+	"log"
+)
+
+type HuomaoClient struct {
+	url string
+}
+
+func (h *HuomaoClient) Run() {
+
+}
+
+func (h *HuomaoClient) loadConfig() {
+
+}
+
+func (h *HuomaoClient) initSocket() {
+
+}
+
+func (h *HuomaoClient) parse(data []byte) {
+
+}
