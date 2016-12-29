5f9c103c8eea543a6b6982040e8dc854262b5466
yxd123 <176912231@163.com>
2016-12-30 01:11:19 +0800

    update base struct

--- danmu/danmu.go
+++ danmu/danmu.go
@@ -1,21 +1,34 @@
 package danmu
 
+import (
+	"log"
+	"regexp"
+)
+
 type IDanmuClient interface {
-	//
 	Add()
 	Has() bool
 	Online() bool
-	//
 	Run()
 	Prepare()
 	Connect()
-	PushMsg()
-	ReadMsg()
+	PushMsg(msg []byte)
+	PullMsg(callback func(msg []byte))
 }
 
 type Danmu struct {
 	channel chan int
 	clients map[int]interface{}
+}
+
+func (d *Danmu) match(roomUrl string) IDanmuClient {
+	reg := regexp.MustCompile(`.*\W+(\w*)\.[tvcom]{2,3}.*`)
+	key := reg.FindString(roomUrl)
+	if _, ok := d.clients[key]; ok {
+		return d.clients[key]
+	}
+
+	return nil
 }
 
 func New(channel chan int) *Danmu {
@@ -34,4 +47,14 @@ func New(channel chan int) *Danmu {
 
 func (d *Danmu) Push(roomUrl string) {
 	roomUrl = TrimUrl(roomUrl)
+	key := GenRoomKey(roomUrl)
+	for _, client := range d.clients {
+		if _, ok := client[key]; ok {
+			log.Println("url exist:", roomUrl)
+			return
+		}
+	}
+
+	client := d.match(roomUrl)
+	client.Add(roomUrl)
 }
